9fa673f664ec8973590e45cc8c8e24dcf5bfc1d4
Inox Danil <inoxdanil@gmail.com>
2024-01-31 09:18:51 +0600

    all hosts

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9bi-osl)%parv4=o#5#7iez7vy$*-iox!%x5ot#^$y+^l)8=cd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
